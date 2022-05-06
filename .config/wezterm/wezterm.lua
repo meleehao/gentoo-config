a93fa7c3b48e7142721cfee9ea5a0bb07df03a83
meleehao <meleehao@outlook.com>
2022-05-06 19:59:44 +0800

    second update gentoo linux for laptop configure

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ return {
   -- for example, this selects a Bold, Italic font variant.
   font = wezterm.font("JetBrains Mono", {weight="Bold", italic=true}),
   font_size = 14,
-  color_scheme = "Ayu Mirage",
+  color_scheme = "iceberg-dark",
   enable_tab_bar = false,
   initial_cols = 110,
   initial_rows = 30,
